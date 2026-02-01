f2150e2e8a6a001108b17667b0e0dc9f06e4697e
kanbs <Kartiknagare3165@gmail.com>
2026-02-01 23:24:23 +0530

    done 2

---
+++ 1. Arrays/duplicateNumber.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def findDuplicate(self, nums):
+        slow = nums[0]
+        fast = nums[0]
+
+        # Phase 1: detect cycle
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+
+        # Phase 2: find entrance
+        slow = nums[0]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+
+        return slow
